962aede63ad1bfa1c4063d1e9130a677dc34ae4f
andregabriel23 <andregabrielvalenza@gmail.com>
2019-11-08 18:04:49 -0500

    subir las etiquetas del blog

--- src/app/tema/blog-detalle/blog-detalle.component.ts
+++ src/app/tema/blog-detalle/blog-detalle.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { DatabaseService } from '../../../services/database.service';
 import { Router, ActivatedRoute } from '@angular/router';
 import * as moment from 'moment';
+import { DialogTextComponent } from '../../dialogs/dialog-text/dialog-text.component';
+import { DialogImageComponent } from '../../dialogs/dialog-image/dialog-image.component';
+import { MatDialog, MatDialogConfig } from "@angular/material"
 
 @Component({
   selector: 'app-blog-detalle',
@@ -10,10 +13,13 @@ import * as moment from 'moment';
 })
 export class BlogDetalleComponent implements OnInit {
 detalle:any;
+  etiquetas: any;
+  imagenes: any;
   constructor(
     public db:DatabaseService,
     public route: Router,
-    private activatedRoute: ActivatedRoute
+    private activatedRoute: ActivatedRoute,
+    private dialog: MatDialog
   ) { 
     
   }
@@ -29,7 +35,63 @@ detalle:any;
           console.log('Este es el detalle:',this.detalle);
         });
     } )
+
+    let idioma: string;
+    idioma = localStorage.getItem("idioma");
+
+    if (idioma === undefined || idioma === null) {
+      idioma = 'es';
+    }
+
+    this.db.getPaginaWebEtiquetas ('blog_detalle_' + idioma).subscribe ((res) => {
+      this.etiquetas = res;
+    });
+
+    this.db.getPaginaWebEtiquetas ('blog_detalle').subscribe ((res) => {
+      this.imagenes = res;
+      console.log ("res", res);
+    });
+
+
   }
+
+  openDialog (etiqueta_key: string, etiqueta_valor: string, tipo_entry: number) {
+    const dialogConfig = new MatDialogConfig();
+
+    dialogConfig.disableClose = true;
+    dialogConfig.autoFocus = true;
+    dialogConfig.height = '500px';
+    dialogConfig.width = '600px';
+
+    let idioma: string;
+    idioma = localStorage.getItem ("idioma");
+
+    if (idioma === undefined || idioma === null) {
+      idioma = 'es';
+    }
+
+    dialogConfig.data = {
+      etiqueta_key: etiqueta_key,
+      etiqueta_valor: etiqueta_valor,
+      tipo_entry: tipo_entry,
+      doc: 'blog_detalle',
+      idioma: idioma
+    };
+
+    this.dialog.open (DialogTextComponent, dialogConfig);
+  }
+
+  openDialogImagen (etiqueta_key: string, etiqueta_valor: any) {
+    const dialogConfig = new MatDialogConfig();
+
+    dialogConfig.data = {
+      etiqueta_key: etiqueta_key,
+      etiqueta_valor: etiqueta_valor
+    };
+
+    this.dialog.open (DialogImageComponent, dialogConfig);
+  }
+
   formatoFecha1(fecha:string)
   {
     return moment(fecha).format('L');
--- src/app/tema/contacto/contacto.component.ts
+++ src/app/tema/contacto/contacto.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { DatabaseService } from '../../../services/database.service';
 import * as $ from 'jquery';
+import { DialogTextComponent } from '../../dialogs/dialog-text/dialog-text.component';
+import { DialogImageComponent } from '../../dialogs/dialog-image/dialog-image.component';
+import { MatDialog, MatDialogConfig } from "@angular/material"
+
 @Component({
   selector: 'app-contacto',
   templateUrl: './contacto.component.html',
@@ -10,8 +14,11 @@ export class ContactoComponent implements OnInit {
   Categorias:any[]=[];
   Faqs:any[]=[];
   idcat:any;
+  etiquetas: any;
+  imagenes: any;
   constructor(
-    public db:DatabaseService
+    public db:DatabaseService,
+    private dialog: MatDialog
   ) { }
 
   ngOnInit() {
@@ -42,7 +49,62 @@ export class ContactoComponent implements OnInit {
       }
       
     });
+    let idioma: string;
+    idioma = localStorage.getItem("idioma");
+
+    if (idioma === undefined || idioma === null) {
+      idioma = 'es';
+    }
+
+    this.db.getPaginaWebEtiquetas ('contacto_' + idioma).subscribe ((res) => {
+      this.etiquetas = res;
+    });
+
+    this.db.getPaginaWebEtiquetas ('contacto').subscribe ((res) => {
+      this.imagenes = res;
+      console.log ("res", res);
+    });
+
   }  
+
+  openDialog (etiqueta_key: string, etiqueta_valor: string, tipo_entry: number) {
+    const dialogConfig = new MatDialogConfig();
+
+    dialogConfig.disableClose = true;
+    dialogConfig.autoFocus = true;
+    dialogConfig.height = '500px';
+    dialogConfig.width = '600px';
+
+    let idioma: string;
+    idioma = localStorage.getItem ("idioma");
+
+    if (idioma === undefined || idioma === null) {
+      idioma = 'es';
+    }
+
+    dialogConfig.data = {
+      etiqueta_key: etiqueta_key,
+      etiqueta_valor: etiqueta_valor,
+      tipo_entry: tipo_entry,
+      doc: 'contacto',
+      idioma: idioma
+    };
+
+    this.dialog.open (DialogTextComponent, dialogConfig);
+  }
+
+  openDialogImagen (etiqueta_key: string, etiqueta_valor: any) {
+    const dialogConfig = new MatDialogConfig();
+
+    dialogConfig.data = {
+      etiqueta_key: etiqueta_key,
+      etiqueta_valor: etiqueta_valor
+    };
+
+    this.dialog.open (DialogImageComponent, dialogConfig);
+  }
+
+
   TraerFaqs (id:string) {
     this.idcat=id;
     this.db.getFaqsporCats(id).subscribe( data => 
